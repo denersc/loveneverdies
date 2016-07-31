1c01ee4c2433660966e7581a30eef305a69e07db
denersc <dener@gmail.com>
2016-07-31 02:35:10 +0000

    Dinamicas unicornianos quase-prontas

--- src/game.lua
+++ src/game.lua
@@ -1,6 +1,7 @@
 gamestate = require "src.gamestate"
 local Enemy = require "src.Enemy"
 local Astronaut = require "src.Astronaut"
+local Unicorn = require "src.Unicorn"
 Bullet = require "src.Bullet"
 
 local game = {}
@@ -17,6 +18,9 @@ function game:enter()
    -- creating enemies
    enemy1 = Enemy(15,50)
    enemy2 = Enemy(680,550)
+   
+   -- creating a unicorn
+   unicorn1 = Unicorn(500,500,0)
 
    -- creating astronaut
    astronautSpeed = 250
@@ -35,6 +39,7 @@ function game:update(dt)
    enemy1:update(dt, astronaut:getHorizontalPosition() + astronaut:getImage():getWidth()/2, astronaut:getVerticalPosition() + astronaut:getImage():getHeight()/2)
    enemy2:update(dt, astronaut:getHorizontalPosition() + astronaut:getImage():getWidth()/2, astronaut:getVerticalPosition() + astronaut:getImage():getHeight()/2)
 
+   unicorn1:update(dt, astronaut)
 end
 
 -- Function Draw --
@@ -46,6 +51,8 @@ function game:draw()
 
    enemy1:draw()
    enemy2:draw()
+   
+   unicorn1:draw(backgroundx, backgroundy)
 end
 
 
