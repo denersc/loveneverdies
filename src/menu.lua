66932b604450d719c844203ea715aa310e5f0ee4
Logan <joaospuri1@hotmail.com>
2016-07-31 14:19:00 -0300

    added menu

--- src/menu.lua
+++ src/menu.lua
@@ -6,11 +6,28 @@ local space = require "src.space"
 local menu = {}
 
 function menu:enter()
-    gamestate.switch(space)
+    start = love.graphics.newImage("assets/start.png")
+    about = love.graphics.newImage("assets/about.png")
+    menuBackground = love.graphics.newImage("assets/menu-fundo.png")
+    
+    --gamestate.switch(space)
 end
 
 function menu:draw()
+    love.graphics.draw(menuBackground, 0, 0)
+    love.graphics.draw(start, 40, 300)
+    love.graphics.draw(about, 600, 300)
+end
 
+function menu:update(dt)
+    if love.mouse.isDown("1") then
+       x = love.mouse.getX()
+       y = love.mouse.getY()
+       
+       if (x > 40 and x < 200) and (y > 300 and y < 415) then
+           gamestate.switch(space)
+       end
+    end
 end
 
 function menu:leave()
