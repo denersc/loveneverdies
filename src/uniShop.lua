9f60dfc9432b286d5b3c5726d5267dee1f9fc804
denersc <dener@gmail.com>
2016-07-31 13:40:45 +0000

    Astronauta animado, começo do shop na terra

---
+++ src/uniShop.lua
@@ -0,0 +1,65 @@
+gamestate = require "src.gamestate"
+local uniShop{}
+
+currentMission = {}
+currentMissionNumber = nil
+currentMissionComplete = false
+
+function uniShop:enter()
+	if currentMissionNumber == nil then
+		currentMissionNumber = 1
+		uniShop:assignMission()
+	end
+end
+
+function uniShop:update()
+	if currentMissionComplete == true and 
+
+end
+
+function uniShop:draw()
+	if currentMissionComplete == true then
+		--Desenha botão de terminar missão
+	end
+
+end
+
+function uniShop:leave()
+
+end
+
+function uniShop:assignMission()
+	if currentMissionNumber == 1 then
+		currentMission[0] = math.random(2,4)
+		currentMission[1] = math.random(1,3)
+		currentMission[2] = math.random(0,2)
+		currentMission[3] = math.random(0,1)
+		currentMission[4] = 0
+	elseif currentMissionNumber == 2 then
+		currentMission[0] = math.random(3,4)
+		currentMission[1] = math.random(2,3)
+		currentMission[2] = math.random(1,3)
+		currentMission[3] = math.random(1,2)
+		currentMission[4] = 0
+	elseif currentMissionNumber == 3 then
+		currentMission[0] = math.random(3,5)
+		currentMission[1] = math.random(3,4)
+		currentMission[2] = math.random(2,3)
+		currentMission[3] = 3
+		currentMission[4] = 1
+	elseif currentMissionNumber == 4 then
+		currentMission[0] = math.random(3,7)
+		currentMission[1] = math.random(4,8)
+		currentMission[2] = math.random(5,8)
+		currentMission[3] = math.random(3,5)
+		currentMission[4] = 3
+	elseif currentMissionNumber == 5 then
+		currentMission[0] = 10
+		currentMission[1] = 10
+		currentMission[2] = 10
+		currentMission[3] = 10
+		currentMission[4] = 10
+	end
+end
+
+return uniShop
